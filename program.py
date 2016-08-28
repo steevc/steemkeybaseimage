e5c5a62143dc4a32bb201e5c3e0eb0dbabc87863
steve <steve@bagofspoons.net>
2016-08-28 11:48:38 +0100

    Attempted copying to clipboard, but issues with pip

--- program.py
+++ program.py
@@ -1,6 +1,7 @@
 import os, shutil, sys
 from datetime import date
 from PIL import Image
+#from tkinter import Tk
 
 maximage = 1000
 keybase_local_folder = "/keybase/public/steevc/steemit"
@@ -8,6 +9,7 @@ keybase_web_folder = "https://steevc.keybase.pub/steemit/"
 
 
 def get_filename(filename, description):
+    # Build name using date and description
     name, suffix = os.path.splitext(filename)
     newname = date.today().isoformat() + description.replace(" ", "") + suffix
     #print(newname)
@@ -15,6 +17,7 @@ def get_filename(filename, description):
 
 
 def move_file(sourcefile, target):
+    # Move to target location
     shutil.copy(sourcefile, target)
 
 
@@ -32,9 +35,19 @@ def resize_image(image, target):
     smimage.save(target)
 
 
+# Issues with pip to install packages
+#def text_to_clipboard(text):
+#    # From http://stackoverflow.com/questions/11063458/python-script-to-copy-text-to-clipboard
+#    r = Tk()
+#    r.withdraw()
+#    r.clipboard_clear()
+#    r.clipboard_append(text)
+#    r.destroy()
+
+
 def process(filename, description):
     im = Image.open(filename)
-    print(im.size)
+    #print(im.size)
     newname = get_filename(filename, description)
     target = os.path.join(keybase_local_folder, newname)
     if im.size[0] > maximage or im.size[1] > maximage:
@@ -46,6 +59,7 @@ def process(filename, description):
     print("Web file", web_file)
     steemit_md = "![" + description + "](" + web_file + ")"
     print(steemit_md)
+    #text_to_clipboard(steemit_md)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
